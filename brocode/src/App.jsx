a2be1e15d9698f6c39474c08168a5eb06ace642c
Mohit Kumar <mohitkumar291202@gmail.com>
2026-01-20 21:50:32 +0530

    Update React code

--- brocode/src/App.jsx
+++ brocode/src/App.jsx
@@ -4,12 +4,16 @@ import Food from './food.jsx';*/
 
 import Card from './Card.jsx';
 import Button from './Button.jsx';
+import Student from './Students.jsx'; 
 function App() {
     return (
     <>
         <Card/>
         <Button/>
         <Button/>
+        <Student name="Mohit" age={20}/>
+        <Student name="Rohit" age={22}/>
+        <Student name="Mike" age={25}/>
     </>
         
     );
