5a026ad9c3512198e5a5c27d003fa9b50453ab42
Mohit Kumar <mohitkumar291202@gmail.com>
2026-01-31 14:19:36 +0530

    updated code for updater function

--- brocode/src/Temp.jsx
+++ brocode/src/Temp.jsx
@@ -1,42 +1,32 @@
-import React, {useState} from 'react';  
+import React, { useState } from 'react';
 
+function MyComponent() {
 
-function MyComponent(){
-    const [name, setName] = useState("guest");
-    const [age,setAge] = useState("");
-    const [payment, setPayment] = useState("");
+    const [count, setCount] = useState(0);
 
+    function increment(){
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+    };
+ 
+    function decrement(){
+        setCount(c => c - 1);
+        setCount(c => c - 1);
+        setCount(c => c - 1);
+    };
 
-    function handleNameChange(event){
-        setName(event.target.value);
-        
+    function reset(){
+        setCount(0);
     }
 
-    function handleChangeAge(event){
-        setAge(event.target.value);
-        
-    }
-
-    function handleChangePayment(event){
-        setPayment(event.target.value);
-    }
-
-    return( <div>
-                <p>Name: {name}</p>
-                <input type="text" value={name} onChange={handleNameChange}/>
-                
-
-                <p>Age : {age}</p> 
-                <input type="number" value={age} onChange={handleChangeAge}/>   
-                
-
-                <p>Payment mode:{payment}</p>
-                <select value={payment} onChange= {handleChangePayment}>
-                    <option value="Credit Card">CREDIT CARD</option>
-                    <option value="Debit Card">DEBIT CARD</option>
-                    <option value="PayPal">PAYPAL</option>
-                </select>    
-                
-            </div>);
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={increment}>Increment</button>
+        </div>
+      );
 }
 export default MyComponent
