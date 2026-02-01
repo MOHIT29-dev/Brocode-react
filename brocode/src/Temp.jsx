044f19048d75875f9b5d2e62a78fd64189bbe388
Mohit Kumar <mohitkumar291202@gmail.com>
2026-02-01 12:47:06 +0530

    Comment added via Git GUI.

--- brocode/src/Temp.jsx
+++ brocode/src/Temp.jsx
@@ -64,3 +64,9 @@ export default MyComponent;
 // git add .
 // git commit -m "message"
 // git push origin branch_name
+
+//rollback to previous commit
+// git checkout commit_id
+// git checkout main (to go back to main branch)
+// git merge commit_id (to merge changes from commit_id to main branch)
+
