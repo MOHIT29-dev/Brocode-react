65a1a026e9ce796c6cc9a6136f35bffe8677acf0
Mohit Kumar <mohitkumar291202@gmail.com>
2026-02-02 11:13:33 +0530

    useRef example code click-3 function updated

--- brocode/src/Temp.jsx
+++ brocode/src/Temp.jsx
@@ -31,6 +31,12 @@ function MyComponent(){
         inputRef3.current.style.backgroundColor = "";
     }
 
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "yellow";
+    }
 
     return (
         <div>
